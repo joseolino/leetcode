e38c721f936ace94a16e4afe879593b6822b140d
joseolino <joseolino@gmail.com>
2022-12-11 13:52:39 -0300

    non-optimal version

--- LC003.py
+++ LC003.py
@@ -6,16 +6,18 @@ class Solution:
         streak = 0
         while i < len(s):
             if(s[i] in hashmap.keys()):
-                i = hashmap[s[i]] + 1
                 max_streak = streak if streak > max_streak else max_streak
+                i = hashmap[s[i]] + 1
+                hashmap = {}
                 streak = 0
             else:
                 hashmap[s[i]] = i
                 i += 1
                 streak += 1
+        max_streak = streak if streak > max_streak else max_streak
         return max_streak
 
 
 s = Solution()
 
-print(s.lengthOfLongestSubstring("abb"))
+print(s.lengthOfLongestSubstring("aab"))
